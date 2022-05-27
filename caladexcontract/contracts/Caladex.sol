d4d269a5e3c7cd31a3c4c64ace7a7297f9c56e64
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-05-27 19:38:52 +0800

    approve for withdraw to right person

--- caladexcontract/contracts/Caladex.sol
+++ caladexcontract/contracts/Caladex.sol
@@ -8,9 +8,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Caladex is Ownable{
 
     address public storeAddress;
+    mapping (address => mapping (address => uint256)) public approvedWithdrawAmountPerToken;
+    mapping (address => uint256) public approvedWithdrawAmountETH;
 
     constructor() {
-        storeAddress = address(0x3dF6149A499A6248CA756e611b849D912058C435);
+        storeAddress = address(0xe2B8ccb724928A6E59480a2EaeDF45bEf0BC3a2f);
     }
 
     function sendViaTransfer() public payable {
@@ -23,12 +25,22 @@ contract Caladex is Ownable{
         require(IERC20(_token).balanceOf(msg.sender) >= amount, "underflow balance recipient");
         require(IERC20(_token).transferFrom(msg.sender, storeAddress, amount), "Failed to re turn tokens to the investor");
     }
+    function approveWithdrawETH(address _to, uint256 amount) public payable{
+        approvedWithdrawAmountETH[_to].add(amount);
+    }
     function withdrawETH(address _to, uint256 amount) public payable{
+        require(approvedWithdrawAmountETH[_to] >= amount, "Invalid allowance");
+        approvedWithdrawAmountETH[_to].sub(amount);
         payable(_to).transfer(amount);
+    }
+    function approveWithdraw(address _to, address _token, uint256 amount) public {
+        approvedWithdrawAmountPerClient[_to][_token].add(amount);
     }
     function withdraw(address _to, address _token, uint256 amount) public{
         require(IERC20(_token).allowance(storeAddress,address(this)) >= amount, "Invalid allowance");
-        require(IERC20(_token).transferFrom(storeAddress, address(_to), amount), "Failed to return tokens to the investor");
+        require(approvedWithdrawAmountPerClient[_to][_token] >= amount, "Invalid allowance");
+        approvedWithdrawAmountPerClient[_to][_token].sub(amount);
+        IERC20(_token).transferFrom(storeAddress, address(_to), amount);
     }
     function setStoreAddress(address _newStoreAddress) public onlyOwner {
         storeAddress = _newStoreAddress;
